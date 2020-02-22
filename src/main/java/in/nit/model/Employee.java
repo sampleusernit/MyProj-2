434cccb9e0c453cbe61c9a772bea109e7344d7a1
parihar241 <pariharpiyush17@gmail.com>
2020-02-22 13:27:23 +0530

    Emp 6865 Model Class

---
+++ src/main/java/in/nit/model/Employee.java
@@ -0,0 +1,54 @@
+package in.nit.model;
+
+public class Employee {
+	
+	private Integer empId;
+	private String empName;
+	private Double empSal;
+	private String empDept;
+	
+	public Employee() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empDept=" + empDept
+				+ "]";
+	}
+
+	private Integer getEmpId() {
+		return empId;
+	}
+
+	private void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	private String getEmpName() {
+		return empName;
+	}
+
+	private void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	private Double getEmpSal() {
+		return empSal;
+	}
+
+	private void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+
+	private String getEmpDept() {
+		return empDept;
+	}
+
+	private void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+		
+	     
+
+}
